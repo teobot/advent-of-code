db4a6c101e7ac35e8970aa5ede89c590223ca865
teobot <theoclapperton@outlook.com>
2021-10-05 19:57:33 +0100

    Added common function

--- common/common.js
+++ common/common.js
@@ -1,14 +1,23 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 
 const returnInput = (filename) => {
-    return fs.readFileSync(path.resolve(__dirname, filename), 'utf-8')
-}
+  return fs.readFileSync(path.resolve(__dirname, filename), "utf-8");
+};
 
 const inputToArray = (filename, splitter) => {
-    return returnInput(filename).split(splitter)
-}
+  return returnInput(filename).split(splitter);
+};
+
+const inputToIntArray = (filename, split) => {
+  return returnInput(filename)
+    .split(split)
+    .map(function (item) {
+      return parseInt(item, 10);
+    });
+};
 
 module.exports = {
-    inputToArray
-}
+  inputToArray,
+  inputToIntArray
+};
