f331e8e2428aee74031384ff3ff7e8107b8322a5
Theo Clapperton <theoclapperton@outlook.com>
2021-10-25 13:12:14 +0100

    Attempted 2017 task 3

---
+++ 2017/task 3/main.js
@@ -0,0 +1,35 @@
+// Advent of code 2017 -- Filename : main.js
+// Created by Theo Clapperton
+// Started @ 2021-10-19 14:39:47
+
+const { inputToArray } = require("../../common/common");
+
+const _REALinput = 368078;
+
+const _TESTinput1 = 1;
+const _TESTinput2 = 12;
+const _TESTinput3 = 23;
+const _TESTinput4 = 1024;
+
+const part1 = (input) => {
+  // part 1
+  let wrap = 0;
+
+  console.log(surroundingValues);
+
+  return 0;
+};
+
+const part2 = (input) => {
+  // part 2
+
+  return 0;
+};
+
+console.log({ task: 1, value: part1(_TESTinput1) });
+console.log({ task: 1, value: part1(_TESTinput2) });
+console.log({ task: 1, value: part1(_TESTinput3) });
+console.log({ task: 1, value: part1(_TESTinput4) });
+// console.log({task: 1, value: part1(_REALinput)});
+
+console.log({ task: 2, value: part2(_REALinput) });
