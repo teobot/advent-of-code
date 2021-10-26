00a9aad9dd14efeaab276fadaab33a24066bf6a7
Theo Clapperton <theoclapperton@outlook.com>
2021-10-26 14:33:48 +0100

    Added new testing methods

--- common/common.js
+++ common/common.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const chalk = require("chalk");
 
 const returnInput = (filename) => {
   return fs.readFileSync(path.resolve(__dirname, filename), "utf-8");
@@ -17,7 +18,36 @@ const inputToIntArray = (filename, split) => {
     });
 };
 
+function calculatePercentage(part, total) {
+  return Math.round((part / total) * 100);
+}
+
+const tests = (tests) => {
+  console.clear();
+  let passedTests = 0;
+  for (let i = 0; i < tests.length; i++) {
+    const { f, input, expected } = tests[i];
+    let result = f(input);
+    let passedOrFail = result === expected;
+    console.log(chalk`
+      \tTest {underline #${i + 1}} {${
+      passedOrFail ? "green.bold passed" : "red.bold failed"
+    }}.
+      \tExpected {green.bold ${expected}} ${
+      passedOrFail ? "and" : "but"
+    } got {${passedOrFail ? "green" : "red"}.bold ${result}}.
+    `);
+    if (passedOrFail) passedTests++;
+  }
+  console.log(chalk`
+    \tPassed {green.bold ${passedTests}} out of {red.bold ${
+    tests.length
+  }} - ${calculatePercentage(passedTests, tests.length)}%.
+  `);
+};
+
 module.exports = {
   inputToArray,
-  inputToIntArray
+  inputToIntArray,
+  tests,
 };
