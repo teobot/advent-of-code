6c9973ca973b11f928c3a6a8a9a5eb8d583b3065
teobot <theoclapperton@outlook.com>
2021-12-13 16:25:51 +0000

    init boilerplate

---
+++ 2021/task 12/main.js
@@ -0,0 +1,25 @@
+// Advent of code 2021 -- Filename : main.js
+// Task link -- https://adventofcode.com/2021/day/12
+// Github link -- https://github.com/teobot/advent-of-code/tree/main/2021/task%2012
+// Created by Theo Clapperton
+
+const { inputToArray, tests } = require("../../common/common");
+
+const _REALinput = inputToArray("../2021/task 12/input.txt", "\n");
+
+const part1 = (input) => {
+    // part 1
+    
+    return 0;
+}
+
+const part2 = (input) => {
+    // part 2
+    
+    return 0;
+}
+
+tests([
+    { f: part1, input: [], expected: 0 },
+    { f: part2, input: [], expected: 0 },
+]);
