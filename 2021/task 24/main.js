4698e0c8b6765c1e9ac468d2d87e833c4fb9a43d
theo.clapperton <theoclapperton@outlook.com>
2021-12-30 23:37:10 +0000

    added attempted code

--- 2021/task 24/main.js
+++ 2021/task 24/main.js
@@ -6,23 +6,109 @@
 const { inputToArray, tests } = require("../../common/common");
 
 const _REALinput = inputToArray("../2021/task 24/input.txt", "\n");
-const _TESTinput = inputToArray("../2021/task 24/test.txt", "\n");
 
-const part1 = (input) => {
-    // part 1
-    
-    return 0;
+function isNumeric(str) {
+  if (typeof str != "string") return false; // we only process strings!
+  return (
+    !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+    !isNaN(parseFloat(str))
+  ); // ...and ensure strings of whitespace fail
 }
+
+function percentage(partialValue, totalValue) {
+  return Math.round((100 * partialValue) / totalValue);
+}
+
+const part1 = (inp) => {
+  // part 1
+
+  let highest = 0;
+
+  const data = inp.map((line) => {
+    let [action, val1, val2] = line.split(" ");
+    return {
+      action,
+      val1,
+      val2: isNumeric(val2) ? parseInt(val2) : val2,
+      val2Numeric: isNumeric(val2),
+    };
+  });
+
+  for (let val = 99999999999999; val > 11111111111111; val--) {
+    if (`${val}`.includes("0")) continue;
+
+    let number = `${val}`.split("").map(Number);
+    let values = { w: 0, x: 0, y: 0, z: 0 };
+
+    data.forEach((instruction) => {
+      switch (instruction.action) {
+        case "inp":
+          values[instruction.val1] = number.shift();
+          break;
+        case "add":
+          values[instruction.val1] =
+            values[instruction.val1] + instruction.val2Numeric
+              ? instruction.val2
+              : values[instruction.val2];
+          break;
+        case "mul":
+          values[instruction.val1] =
+            values[instruction.val1] * instruction.val2Numeric
+              ? instruction.val2
+              : values[instruction.val2];
+          break;
+        case "div":
+          Math.floor(
+            (values[instruction.val1] /= instruction.val2Numeric
+              ? instruction.val2
+              : values[instruction.val2])
+          );
+          break;
+        case "mod":
+          values[instruction.val1] %= instruction.val2Numeric
+            ? instruction.val2
+            : parseInt(values[instruction.val2]);
+          break;
+        case "eql":
+          if (instruction.val2Numeric) {
+            // if the value is numeric, we compare the value to the value
+            if (values[instruction.val1] === instruction.val2) {
+              values[instruction.val1] = 1;
+            } else {
+              values[instruction.val1] = 0;
+            }
+          } else {
+            // if the value is not numeric, we compare the value to the value
+            if (values[instruction.val1] === values[instruction.val2]) {
+              values[instruction.val1] = 1;
+            } else {
+              values[instruction.val1] = 0;
+            }
+          }
+          break;
+        default:
+          break;
+      }
+    });
+
+    if (values.z === 0) {
+      console.log({ highest, values });
+      return highest;
+    }
+  }
+
+  return highest;
+};
 
 const part2 = (input) => {
-    // part 2
-    
-    return 0;
-}
+  // part 2
+
+  return 0;
+};
 
 tests([
-    { f: part1, input: _TESTinput, expected: 0 },
-    //{ f: part1, input: _REALinput, expected: 0 },
-    //{ f: part2, input: _TESTinput, expected: 0 },
-    //{ f: part2, input: _REALinput, expected: 0 },
+  { f: part1, input: _REALinput, expected: 0 },
+  //{ f: part1, input: _REALinput, expected: 0 },
+  //{ f: part2, input: _TESTinput, expected: 0 },
+  //{ f: part2, input: _REALinput, expected: 0 },
 ]);
--- 2021/task 25/main.js
+++ 2021/task 25/main.js
@@ -6,23 +6,136 @@
 const { inputToArray, tests } = require("../../common/common");
 
 const _REALinput = inputToArray("../2021/task 25/input.txt", "\n");
-const _TESTinput = inputToArray("../2021/task 25/test.txt", "\n");
+const _TESTinput = [
+  "v...>>.vv>",
+  ".vv>>.vv..",
+  ">>.>v>...v",
+  ">>v>>.>.v.",
+  "v>v.vv.v..",
+  ">.>>..v...",
+  ".vv..>.>v.",
+  "v.v..>>v.v",
+  "....v..v.>",
+];
+
+// const part1 = (map) => {
+//   let i = 0;
+//   let changed = true;
+//   const W = map[0].length;
+//   const H = map.length;
+//   while (changed) {
+//     changed = false;
+//     for (let turn of [0, 1]) {
+//       const newMap = map.map((row) => row.map((cell) => cell));
+//       map.forEach((row, y) =>
+//         row.forEach((cell, x) => {
+//           if (turn === 0 && cell === ">") {
+//             if (map[y][(x + 1) % W] === ".") {
+//               newMap[y][x] = ".";
+//               newMap[y][(x + 1) % W] = ">";
+//               changed = true;
+//             }
+//           } else if (turn === 1 && cell === "v") {
+//             if (map[(y + 1) % H][x] === ".") {
+//               newMap[y][x] = ".";
+//               newMap[(y + 1) % H][x] = "v";
+//               changed = true;
+//             }
+//           }
+//         })
+//       );
+//       map = newMap;
+//     }
+//     i++;
+//   }
+
+//   return i;
+// };
 
 const part1 = (input) => {
-    // part 1
-    
-    return 0;
-}
+  // part 1
+  let moving = true;
+
+  let data = input.map((row) => {
+    return row.split("").map((char) => {
+      return { char, moved: false };
+    });
+  });
+
+  let step = 0;
+  while (moving) {
+    step++;
+    moving = false;
+
+    const newData = data.map((row) => row.map((cell) => cell));
+    for (let y = 0; y < data.length; y++) {
+      for (let x = 0; x < data[y].length; x++) {
+        if (
+          newData[y][x].char === "." ||
+          newData[y][x].char === "v" ||
+          newData[y][x].moved
+        )
+          continue;
+
+        let next_i = x + 1 < newData[y].length ? x + 1 : 0;
+
+        if (newData[y][x].char === ">" && newData[y][next_i].char === ".") {
+          newData[y][next_i].char = ">";
+          newData[y][next_i].moved = true;
+          newData[y][x].char = ".";
+          newData[y][x].moved = false;
+          moving = true;
+        }
+      }
+    }
+
+    for (let y = 0; y < newData.length; y++) {
+      for (let x = 0; x < newData[y].length; x++) {
+        if (
+          newData[y][x].char === "." ||
+          newData[y][x].char === ">" ||
+          newData[y][x].moved
+        )
+          continue;
+
+        let next_row_num = y + 1 < newData.length ? y + 1 : 0;
+
+        if (
+          newData[y][x].char === "v" &&
+          newData[next_row_num][x].char === "."
+        ) {
+          newData[next_row_num][x].char = "v";
+          newData[next_row_num][x].moved = true;
+          newData[y][x].char = ".";
+          newData[y][x].moved = false;
+          moving = true;
+        }
+      }
+    }
+
+    data = newData;
+
+    for (let yy = 0; yy < data.length; yy++) {
+      for (let xx = 0; xx < data[yy].length; xx++) {
+        if (data[yy][xx].moved) {
+          data[yy][xx].moved = false;
+        }
+      }
+    }
+  }
+
+  return step;
+};
 
 const part2 = (input) => {
-    // part 2
-    
-    return 0;
-}
+  // part 2
+
+  return 0;
+};
 
 tests([
-    { f: part1, input: _TESTinput, expected: 0 },
-    //{ f: part1, input: _REALinput, expected: 0 },
-    //{ f: part2, input: _TESTinput, expected: 0 },
-    //{ f: part2, input: _REALinput, expected: 0 },
+  { f: part1, input: _TESTinput, expected: 58 },
+  //{ f: part1, input: _REALinput, expected: 557 },
+  //{ f: part2, input: _TESTinput, expected: 0 },
+  //{ f: part2, input: _REALinput, expected: 0 },
 ]);
