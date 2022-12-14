a7841b5d13ffc15dd75cb76e53f90f7859f54af1
Theo Clapperton <theoclapperton@outlook.com>
2022-12-14 17:26:34 +0000

    updated for part2

--- 2022/task 10/main.js
+++ 2022/task 10/main.js
@@ -39,15 +39,20 @@ const part1 = (input) => {
   return total;
 };
 
+let crtGrid = [
+  "........................................",
+  "........................................",
+  "........................................",
+  "........................................",
+  "........................................",
+  "........................................",
+].map((row) => row.split(""));
+
 const part2 = (input) => {
   // part 2
 
   let counter = 1;
-  let total = 0;
-  let nthCycle = 20;
-  let x = 1;
-
-  input
+  let commands = input
     .map((command) => {
       let ii = command === "noop" ? counter++ : (counter += 2);
       return {
@@ -56,21 +61,39 @@ const part2 = (input) => {
         val: parseInt(command.split(" ")[1]),
       };
     })
-    .filter((command) => command.com !== "noop")
-    .forEach(({ com, ind, val }) => {
-      if (ind > nthCycle) {
-        total += nthCycle * x;
-        nthCycle += 40;
-      }
-      x += val;
-    });
+    .filter((command) => command.com !== "noop");
+
+  let nthCycle = 40;
+  let row = 0;
+  let column = 0;
+  let x = 1;
+
+  for (let i = 1; i < 241; i++) {
+    let movement = commands.find((command) => command.ind === i);
+    if (movement) {
+      x += movement.val;
+    }
+    if (i > nthCycle) {
+      nthCycle += 40;
+      row++;
+      x = 1;
+      column = 0;
+    }
+
+    if (column === x - 1 || column === x || column === x + 1)
+      crtGrid[row][column] = "#";
+
+    column++;
+  }
+
+  crtGrid.forEach((row) => console.log(row.join("")));
 
   return 0;
 };
 
 tests([
-  test(part1, _TESTinput, 13140),
-  test(part1, _REALinput, 13480),
-  test(part2, _TESTinput, 0),
-  //test(part2, _REALinput, 0)
+  //test(part1, _TESTinput, 13140),
+  //test(part1, _REALinput, 13480),
+  //test(part2, _TESTinput, 0),
+  test(part2, _REALinput, 0)
 ]);
